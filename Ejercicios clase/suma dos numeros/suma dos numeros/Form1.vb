8b93a32cb4de8089fd87a447feb86f63f19e8f7f
walther efrain <waltherulloa95@gmail.com>
2020-05-30 17:47:12 -0600

    suma de dos numero

---
+++ Ejercicios clase/suma dos numeros/suma dos numeros/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'inicio
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim suma As Integer
+
+
+        valor1 = Val(TextBox1.Text)
+        valor2 = Val(TextBox2.Text)
+
+
+        'proceso
+
+
+
+        'salidas
+        suma = valor1 + valor2
+        TextBox3.Text = suma
+
+
+    End Sub
+End Class
