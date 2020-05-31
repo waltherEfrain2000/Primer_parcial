83d862dfdb60295cd8e4fa8fc0792c9b2276e822
walther efrain <waltherulloa95@gmail.com>
2020-05-31 15:30:38 -0600

    factura

---
+++ Ejercicios clase/factura/Factura Sencilla/Form1.Designer.vb
@@ -0,0 +1,299 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class factura
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(factura))
+        Me.gb1 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt2 = New System.Windows.Forms.TextBox()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt6 = New System.Windows.Forms.TextBox()
+        Me.txt5 = New System.Windows.Forms.TextBox()
+        Me.txt4 = New System.Windows.Forms.TextBox()
+        Me.txt3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.gb1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gb1
+        '
+        Me.gb1.Controls.Add(Me.ComboBox2)
+        Me.gb1.Controls.Add(Me.ComboBox1)
+        Me.gb1.Controls.Add(Me.CheckBox2)
+        Me.gb1.Controls.Add(Me.CheckBox1)
+        Me.gb1.Controls.Add(Me.Label2)
+        Me.gb1.Controls.Add(Me.Label1)
+        Me.gb1.Controls.Add(Me.txt2)
+        Me.gb1.Controls.Add(Me.txt1)
+        Me.gb1.ForeColor = System.Drawing.SystemColors.WindowFrame
+        Me.gb1.Location = New System.Drawing.Point(86, 54)
+        Me.gb1.Name = "gb1"
+        Me.gb1.Size = New System.Drawing.Size(217, 229)
+        Me.gb1.TabIndex = 1
+        Me.gb1.TabStop = False
+        Me.gb1.Text = "Datos Requeridos"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"el 15% de impuesto", "el 17% de impuesto"})
+        Me.ComboBox2.Location = New System.Drawing.Point(24, 172)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(160, 21)
+        Me.ComboBox2.TabIndex = 8
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Enabled = False
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Descuento Tercera Edad", "Descuento Cliente regular", "Descuento Tarjeta de credito", "Descuento Diversas compras", "Descuento Publico general", "Descuento cliente premiun"})
+        Me.ComboBox1.Location = New System.Drawing.Point(25, 123)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(160, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.CheckBox2.Location = New System.Drawing.Point(48, 149)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(75, 17)
+        Me.CheckBox2.TabIndex = 6
+        Me.CheckBox2.Text = "Aplicar Isv"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.ForeColor = System.Drawing.SystemColors.WindowText
+        Me.CheckBox1.Location = New System.Drawing.Point(48, 102)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(113, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Aplicar Descuento"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.Label2.Location = New System.Drawing.Point(24, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "cantidad"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.Label1.Location = New System.Drawing.Point(24, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "p.unitario"
+        '
+        'txt2
+        '
+        Me.txt2.Location = New System.Drawing.Point(100, 67)
+        Me.txt2.Name = "txt2"
+        Me.txt2.Size = New System.Drawing.Size(100, 20)
+        Me.txt2.TabIndex = 1
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(100, 41)
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(100, 20)
+        Me.txt1.TabIndex = 0
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txt6)
+        Me.GroupBox1.Controls.Add(Me.txt5)
+        Me.GroupBox1.Controls.Add(Me.txt4)
+        Me.GroupBox1.Controls.Add(Me.txt3)
+        Me.GroupBox1.Location = New System.Drawing.Point(86, 335)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(217, 228)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Detalle Facturas"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(32, 198)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(31, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Total"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 147)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(44, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Isv 15%"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Descuento"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Subtotal"
+        '
+        'txt6
+        '
+        Me.txt6.Location = New System.Drawing.Point(100, 191)
+        Me.txt6.Name = "txt6"
+        Me.txt6.Size = New System.Drawing.Size(100, 20)
+        Me.txt6.TabIndex = 3
+        '
+        'txt5
+        '
+        Me.txt5.Location = New System.Drawing.Point(100, 144)
+        Me.txt5.Name = "txt5"
+        Me.txt5.Size = New System.Drawing.Size(100, 20)
+        Me.txt5.TabIndex = 2
+        '
+        'txt4
+        '
+        Me.txt4.Location = New System.Drawing.Point(100, 88)
+        Me.txt4.Name = "txt4"
+        Me.txt4.Size = New System.Drawing.Size(100, 20)
+        Me.txt4.TabIndex = 1
+        '
+        'txt3
+        '
+        Me.txt3.Location = New System.Drawing.Point(100, 32)
+        Me.txt3.Name = "txt3"
+        Me.txt3.Size = New System.Drawing.Size(100, 20)
+        Me.txt3.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(367, 92)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(367, 126)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Nuevo"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(367, 156)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Salir"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'factura
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(498, 575)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.gb1)
+        Me.Name = "factura"
+        Me.Text = "factura sencilla"
+        Me.gb1.ResumeLayout(False)
+        Me.gb1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents gb1 As GroupBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt6 As TextBox
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ Ejercicios clase/factura/Factura Sencilla/Form1.vb
@@ -0,0 +1,260 @@
+﻿Public Class factura
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txt1.Text = ""
+        txt2.Text = ""
+        txt3.Text = ""
+        txt4.Text = ""
+        txt5.Text = ""
+        txt6.Text = ""
+        CheckBox1.Checked = 0
+        CheckBox2.Checked = 0
+
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+        ComboBox1.Enabled = False
+        ComboBox2.Enabled = False
+
+
+        Try
+            For Each ctl As Control In Me.Controls
+                If TypeOf ctl Is ComboBox Then
+                    ctl.Text = ""
+                End If
+            Next
+        Catch ex As Exception
+            MsgBox("se ha producido un error vuelva pronto")
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim total As Double
+        Const terceraEdad As Double = 0.15
+        Const clienteregular As Double = 0.2
+        Const tarjetaCredito As Double = 0.1
+        Const diversasCompras As Double = 0.08
+        Const publicogeneral As Double = 0.05
+        Const clientepremium As Double = 0.25
+        Dim subtotal, subtotal1, descuentototal, isv As Double
+
+
+
+
+        valor1 = Val(txt1.Text)
+        valor2 = Val(txt2.Text)
+
+        If txt1.Text = "" Then
+            MsgBox(" No se le ovide colocar el precio unitario", vbInformation)
+        ElseIf Not IsNumeric(txt1.Text) Then
+            MsgBox(" Ingrese un valorcorrecto ", vbInformation)
+            txt1.Text = ""
+        End If
+
+        If txt2.Text = "" Then
+            MsgBox(" No se le ovide colocar la cantidad ", vbInformation)
+        ElseIf Not IsNumeric(txt2.Text) Then
+            MsgBox(" Ingrese un valor de cantidad validad ", vbInformation)
+            txt2.Text = ""
+        End If
+        subtotal = valor1 * valor2
+
+
+        If ComboBox1.Text = "Descuento Tercera Edad" Then
+
+            descuentototal = subtotal * terceraEdad
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+        Else
+
+
+        End If
+
+        If ComboBox1.Text = "Descuento Cliente regular" Then
+
+            descuentototal = subtotal * clienteregular
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+        Else
+
+
+        End If
+
+
+        If ComboBox1.Text = "Descuento Tarjeta de credito" Then
+
+            descuentototal = subtotal * tarjetaCredito
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+        Else
+
+
+        End If
+
+        If ComboBox1.Text = "Descuento Diversas compras" Then
+
+            descuentototal = subtotal * diversasCompras
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+        Else
+
+
+        End If
+
+        If ComboBox1.Text = "Descuento Publico general" Then
+
+            descuentototal = subtotal * publicogeneral
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+        Else
+
+
+        End If
+
+        If ComboBox1.Text = "Descuento cliente premiun" Then
+
+            descuentototal = subtotal * clientepremium
+            subtotal1 = subtotal - descuentototal
+            isv = subtotal1 * 0.15
+            total = subtotal1 + isv
+
+
+            txt3.Text = subtotal
+            txt4.Text = descuentototal
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+        Else
+
+
+        End If
+
+        If ComboBox2.Text = "el 15% de impuesto" Then
+            subtotal1 = subtotal * 0.15
+            subtotal = subtotal + subtotal1
+            isv = subtotal * 0.15
+            total = subtotal + isv
+
+            txt3.Text = subtotal
+            txt4.Text = "no hay descuento"
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+
+
+
+
+        End If
+
+        If ComboBox2.Text = "el 17% de impuesto" Then
+            subtotal1 = subtotal * 0.17
+            subtotal = subtotal + subtotal1
+            isv = subtotal * 0.15
+            total = subtotal + isv
+
+            txt3.Text = subtotal
+            txt4.Text = "no hay descuento"
+            txt5.Text = isv
+            txt6.Text = total
+            Button2.Visible = True
+
+
+
+
+
+        End If
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'ComboBox1.Items.Add("Descuento Tercera Edad")
+        'ComboBox1.Items.Add("Descuento Cliente regular")
+        'ComboBox1.Items.Add("Descuento Tarjeta de credito")
+        'ComboBox1.Items.Add("Descuento Diversas compras")
+        'ComboBox1.Items.Add("Descuento Publico general")
+        'ComboBox1.Items.Add("Descuento cliente premiun")
+        CheckBox2.Enabled = False
+        ComboBox1.Enabled = True
+
+
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        '  ComboBox2.Items.Add("el 15% de impuesto")
+        ' ComboBox2.Items.Add("el 17% de impuesto")
+        CheckBox1.Enabled = False
+        ComboBox2.Enabled = True
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        End
+    End Sub
+End Class
